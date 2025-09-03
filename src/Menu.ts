68a737c291b6ecd57ea60ed7d7789b9a84881f9b
Jefferon Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-09-03 11:16:04 -0300

    code formatting

--- src/Menu.ts
+++ src/Menu.ts
@@ -1,31 +1,44 @@
-import { ContaCorrente } from './model/ContaCorrente';
-import { ContaPoupanca } from './model/ContaPoupanca';
-import { colors } from './util/Colors';
-import leia from 'readline-sync';
-
+import { ContaCorrente } from "./model/ContaCorrente";
+import { ContaPoupanca } from "./model/ContaPoupanca";
+import { colors } from "./util/Colors";
+import leia from "readline-sync";
 
 export function main() {
-    
-    let opcao: number;
+  let opcao: number;
 
-    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
-    contacorrente.visualizar();
-    contacorrente.sacar(2000);
-    contacorrente.visualizar();
-    contacorrente.depositar(1000);
-    contacorrente.visualizar();
+  const contacorrente: ContaCorrente = new ContaCorrente(
+    2,
+    123,
+    1,
+    "Mariana",
+    15000,
+    1000
+  );
+  contacorrente.visualizar();
+  contacorrente.sacar(2000);
+  contacorrente.visualizar();
+  contacorrente.depositar(1000);
+  contacorrente.visualizar();
 
-    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
-    contapoupanca.visualizar();
-    contapoupanca.sacar(200);
-    contapoupanca.visualizar();
-    contapoupanca.depositar(1000);
-    contapoupanca.visualizar();
+  const contapoupanca: ContaPoupanca = new ContaPoupanca(
+    3,
+    123,
+    2,
+    "Victor",
+    1000,
+    10
+  );
+  contapoupanca.visualizar();
+  contapoupanca.sacar(200);
+  contapoupanca.visualizar();
+  contapoupanca.depositar(1000);
+  contapoupanca.visualizar();
 
-    while (true) {
-
-        console.log(colors.bg.black, colors.fg.yellow,
-        `
+  while (true) {
+    console.log(
+      colors.bg.black,
+      colors.fg.yellow,
+      `
         **************************************************************
 
                             BANCO GENERATION
@@ -44,84 +57,97 @@ export function main() {
         **************************************************************
 
         Entre com a opção desejada: 
-        `
-        ,colors.reset)
-              
-        opcao = leia.questionInt("");
+        `,
+      colors.reset
+    );
 
-        if (opcao === 9) {
-            console.log(colors.fg.greenstrong, 
-                "\nBanco Generation - O Banco da sua geração!");
-            sobre();
-            console.log(colors.reset, "");
-            process.exit(0);
-        }
+    opcao = leia.questionInt("");
 
-        switch (opcao) {
-            case 1:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nCriar Conta\n\n", colors.reset);
-                
-                keyPress()
-
-                break;
-            case 2:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nListar todas as Contas\n\n", colors.reset);
-
-                keyPress()
-                break;
-            case 3:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
-
-                keyPress()
-                break;
-            case 4:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nAtualizar dados da Conta\n\n", colors.reset);
-
-                keyPress()
-                break;
-            case 5:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nApagar uma Conta\n\n", colors.reset);
-
-                keyPress()
-                break;
-            case 6:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nSaque\n\n", colors.reset);
-
-                keyPress()
-                break;
-            case 7:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nDepósito\n\n", colors.reset);
-
-                keyPress()
-                break;
-            case 8:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nTransferência entre Contas\n\n", colors.reset);
-
-                keyPress()
-                break;
-        
-            default:
-                console.log(colors.fg.whitestrong, 
-                    "\nOpção Inválida!\n", colors.reset);
-
-                keyPress()
-                break;
-        }
+    if (opcao === 9) {
+      console.log(
+        colors.fg.greenstrong,
+        "\nBanco Generation - O Banco da sua geração!"
+      );
+      sobre();
+      console.log(colors.reset, "");
+      process.exit(0);
     }
 
+    switch (opcao) {
+      case 1:
+        console.log(colors.fg.whitestrong, "\n\nCriar Conta\n\n", colors.reset);
+
+        keyPress();
+
+        break;
+      case 2:
+        console.log(
+          colors.fg.whitestrong,
+          "\n\nListar todas as Contas\n\n",
+          colors.reset
+        );
+
+        keyPress();
+        break;
+      case 3:
+        console.log(
+          colors.fg.whitestrong,
+          "\n\nConsultar dados da Conta - por número\n\n",
+          colors.reset
+        );
+
+        keyPress();
+        break;
+      case 4:
+        console.log(
+          colors.fg.whitestrong,
+          "\n\nAtualizar dados da Conta\n\n",
+          colors.reset
+        );
+
+        keyPress();
+        break;
+      case 5:
+        console.log(
+          colors.fg.whitestrong,
+          "\n\nApagar uma Conta\n\n",
+          colors.reset
+        );
+
+        keyPress();
+        break;
+      case 6:
+        console.log(colors.fg.whitestrong, "\n\nSaque\n\n", colors.reset);
+
+        keyPress();
+        break;
+      case 7:
+        console.log(colors.fg.whitestrong, "\n\nDepósito\n\n", colors.reset);
+
+        keyPress();
+        break;
+      case 8:
+        console.log(
+          colors.fg.whitestrong,
+          "\n\nTransferência entre Contas\n\n",
+          colors.reset
+        );
+
+        keyPress();
+        break;
+
+      default:
+        console.log(colors.fg.whitestrong, "\nOpção Inválida!\n", colors.reset);
+
+        keyPress();
+        break;
+    }
+  }
 }
 
 function sobre(): void {
-    console.log(
-        `\n
+  console.log(
+    `\n
         **************************************************************
         
         Projeto Desenvolvido por: Jefferson Carvalho
@@ -130,13 +156,13 @@ function sobre(): void {
 
         **************************************************************
         `
-    )
+  );
 }
 
 function keyPress(): void {
-    console.log(colors.reset, "");
-    console.log("\nPressione enter para continuar...");
-    leia.prompt();
+  console.log(colors.reset, "");
+  console.log("\nPressione enter para continuar...");
+  leia.prompt();
 }
 
 main();
